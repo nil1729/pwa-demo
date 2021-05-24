e0c9d5697217c2530a2607e49e28ac2d34ef7eb7
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2021-05-24 23:59:33 +0530

    strategy

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,20 +1,25 @@
 const dialog = document.querySelector('dialog');
 const showModalButton = document.querySelector('.show-modal');
+const feedContainer = document.querySelector('.feed-container');
+
 if (!dialog.showModal) {
 	dialogPolyfill.registerDialog(dialog);
 }
 showModalButton.addEventListener('click', function () {
 	dialog.showModal();
 });
-
 dialog.querySelector('.close').addEventListener('click', function () {
 	dialog.close();
 });
 
-const feedContainer = document.querySelector('.feed-container');
-function createCard(imgSrc, title, location) {
+function clearCards() {
+	while (feedContainer.hasChildNodes()) feedContainer.removeChild(feedContainer.lastChild);
+}
+
+function createCard(id, imgSrc, title, location) {
 	const feedCard = document.createElement('div');
 	feedCard.className = 'demo-card-wide mdl-card mdl-shadow--2dp';
+	feedCard.id = id;
 
 	// feed title
 	const feedTitle = document.createElement('div');
@@ -31,18 +36,82 @@ function createCard(imgSrc, title, location) {
 	feedLocation.className = 'mdl-card__supporting-text text-center';
 	feedLocation.textContent = location;
 	feedCard.appendChild(feedLocation);
+
+	// Create a Save Button
+	// const saveButton = document.createElement('button');
+	// saveButton.className = 'mdl-button mdl-js-button mdl-button--raised';
+	// saveButton.textContent = 'Save';
+	// feedCard.appendChild(saveButton);
+	// saveButton.addEventListener('click', saveForOfflineAccess(imgSrc));
+
 	componentHandler.upgradeElement(feedCard);
 	feedContainer.appendChild(feedCard);
 }
 
-fetch('https://jsonplaceholder.typicode.com/photos?_limit=5')
+const URL = 'https://jsonplaceholder.typicode.com/photos?_limit=5';
+let networkDataReceived = false;
+
+fetch(URL)
 	.then(function (response) {
+		console.log('From Networks', response);
 		return response.json();
 	})
 	.then(function (data) {
 		if (Array.isArray(data)) {
+			networkDataReceived = true;
+			clearCards();
 			data.forEach(function (item) {
-				createCard(item.url, item.title, 'In India');
+				createCard(item.id, item.url, item.title, 'In India');
 			});
 		}
-	});
+	})
+	.catch(function (err) {});
+
+if (window.caches) {
+	caches
+		.match(URL)
+		.then(function (response) {
+			console.log('From Cache', response);
+			if (response) return response.json();
+		})
+		.then(function (data) {
+			clearCards();
+			if (Array.isArray(data) && !networkDataReceived) {
+				data.forEach(function (item) {
+					createCard(item.id, item.url, item.title, 'In India');
+				});
+			}
+		});
+}
+
+// fetch('https://jsonplaceholder.typicode.com/photos/10')
+// 	.then(function (response) {
+// 		return response.json();
+// 	})
+// 	.then(function (data) {
+// 		createCard(data.id, data.url, data.title, 'In India');
+// 	})
+// 	.catch(function (err) {
+// 		console.error(err);
+// 	});
+
+// Save for Offline Access on demand based on User choice
+// const saveForOfflineAccess = function (photoURL) {
+// 	return function () {
+// 		if (window.caches) {
+// 			caches
+// 				.open('user-requested')
+// 				.then(function (cache) {
+// 					return Promise.all([
+// 						cache.add('https://jsonplaceholder.typicode.com/photos/10'),
+// 						fetch(photoURL, { mode: 'no-cors' }).then(function (res) {
+// 							cache.put(photoURL, res.clone());
+// 						}),
+// 					]);
+// 				})
+// 				.catch(function (err) {
+// 					console.error(err);
+// 				});
+// 		}
+// 	};
+// };
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,27 @@
-const STATIC_CACHE_NAME = 'static-v1';
-const DYNAMIC_CACHE_NAME = 'dynamic-v1';
+const STATIC_CACHE_NAME = 'static-v10';
+const DYNAMIC_CACHE_NAME = 'dynamic-v10';
+const STATIC_FILES = [
+	'/',
+	'/index.html',
+	'/offline.html',
+	'/src/css/app.css',
+	'/src/css/main.css',
+	'/src/js/app.js',
+	'/src/js/feed.js',
+	'/src/js/material.min.js',
+	'/src/images/main-bg.jpg',
+	'https://code.getmdl.io/1.3.0/material.indigo-pink.min.css',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+];
+
+function isInStaticFiles(requestURL, array = STATIC_FILES) {
+	for (let i = 0; i < array.length; i++) {
+		if (requestURL === array[i]) return true;
+		else if (array[i] !== '/' && requestURL.indexOf(array[i]) > -1) return true;
+	}
+	return false;
+}
 
 self.addEventListener('install', function (event) {
 	console.log('[Service Worker] Installing Service Worker...');
@@ -8,19 +30,7 @@ self.addEventListener('install', function (event) {
 			.open(STATIC_CACHE_NAME)
 			.then(function (cache) {
 				console.log('[Service Worker] Caching App Shell...');
-				cache.addAll([
-					'/',
-					'/index.html',
-					'/src/css/app.css',
-					'/src/css/main.css',
-					'/src/js/app.js',
-					'/src/js/feed.js',
-					'/src/js/material.min.js',
-					'/src/images/main-bg.jpg',
-					'https://code.getmdl.io/1.3.0/material.indigo-pink.min.css',
-					'https://fonts.googleapis.com/icon?family=Material+Icons',
-					'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
-				]);
+				cache.addAll(STATIC_FILES);
 			})
 			.finally(function () {
 				self.skipWaiting();
@@ -49,23 +59,130 @@ self.addEventListener('activate', function (event) {
 	);
 });
 
+/**
+ *
+ *
+ * @Strategy Cache with Networks Fallback
+ *
+ */
+// self.addEventListener('fetch', function (event) {
+// 	event.respondWith(
+// 		// First trying to find the resource from cache
+// 		caches.match(event.request).then(function (response) {
+// 			if (response) return response;
+// 			// If not in cache try to load the resource from network
+// 			else {
+// 				return (
+// 					fetch(event.request)
+// 						.then(function (res) {
+// 							return caches.open(DYNAMIC_CACHE_NAME).then(function (cache) {
+// 								cache.put(event.request.url, res.clone());
+// 								return res;
+// 							});
+// 						})
+
+// 						// If network fails or the resource not cached yet
+// 						.catch(function (e) {
+// 							return caches.match('/offline.html');
+// 						})
+// 				);
+// 			}
+// 		})
+// 	);
+// });
+
+/**
+ *
+ *
+ * @Strategy Cache Only
+ *
+ *
+ */
+// self.addEventListener('fetch', function (event) {
+// 	event.respondWith(caches.match(event.request));
+// });
+
+/**
+ *
+ *
+ * @Strategy Network Only
+ *
+ *
+ */
+// self.addEventListener('fetch', function (event) {
+// 	event.respondWith(fetch(event.request));
+// });
+
+/**
+ *
+ *
+ * @Strategy  Network with Cache Fallback
+ *
+ *
+ */
+// self.addEventListener('fetch', function (event) {
+// 	event.respondWith(
+// 		fetch(event.request)
+// 			.then(function (res) {
+// 				return caches.open(DYNAMIC_CACHE_NAME).then(function (cache) {
+// 					cache.put(event.request, res.clone());
+// 					return res;
+// 				});
+// 			})
+// 			.catch(function (err) {
+// 				return caches.match(event.request).then(function (response) {
+// 					if (response) return response;
+// 					else return caches.match('/offline.html');
+// 				});
+// 			})
+// 	);
+// });
+
+/**
+ *
+ *
+ * @Strategy Cache, then Network
+ *
+ *
+ */
 self.addEventListener('fetch', function (event) {
-	event.respondWith(
-		caches.match(event.request).then(function (response) {
-			if (response) return response;
-			else {
+	const URL = 'https://jsonplaceholder.typicode.com/photos?_limit=5';
+
+	if (event.request.url.indexOf(URL) > -1) {
+		event.respondWith(
+			caches.open(DYNAMIC_CACHE_NAME).then(function (cache) {
 				return fetch(event.request).then(function (res) {
-					return caches
-						.open(DYNAMIC_CACHE_NAME)
-						.then(function (cache) {
-							cache.put(event.request.url, res.clone());
-							return res;
-						})
-						.catch(function (e) {
-							console.log(e);
-						});
+					cache.put(event.request, res.clone());
+					return res;
 				});
-			}
-		})
-	);
+			})
+		);
+	} else if (isInStaticFiles(event.request.url)) {
+		event.respondWith(caches.match(event.request));
+	} else {
+		event.respondWith(
+			// First trying to find the resource from cache
+			caches.match(event.request).then(function (response) {
+				if (response) return response;
+				// If not in cache try to load the resource from network
+				else {
+					return (
+						fetch(event.request)
+							.then(function (res) {
+								return caches.open(DYNAMIC_CACHE_NAME).then(function (cache) {
+									cache.put(event.request.url, res.clone());
+									return res;
+								});
+							})
+							// If network fails or the resource not cached yet
+							.catch(function (e) {
+								if (event.request.url.indexOf('/help') > -1) {
+									return caches.match('/offline.html');
+								}
+							})
+					);
+				}
+			})
+		);
+	}
 });
