56a9343c3898aab13f04e1c89082269f186e1eca
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2021-05-27 05:25:56 +0530

    add PUSH Listener Server Side Code

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const serviceAccount = require('./admin.json');
+const webPush = require('web-push');
 const corsOptions = {
 	origin: function (origin, callback) {
-		const allowedOrigins = ['http://localhost:8000', 'https://pwa-bg-sync.web.app'];
+		const allowedOrigins = [
+			'http://localhost:8000',
+			'https://pwa-bg-sync.web.app',
+			'https://pwa-push-notifications.web.app',
+		];
 		console.log('Logged Output ~ file: index.js ~ line 7 ~ Requested Origin', origin);
 		if (allowedOrigins.indexOf(origin) > -1) {
 			callback(null, true);
@@ -15,6 +20,9 @@ const corsOptions = {
 
 const cors = require('cors')(corsOptions);
 const { v4: idGenerator } = require('uuid');
+const vapidPublicKey =
+	'BP4PlsYeQd4Wg6Mqn-o48s6mtktxM2EexAHHBVzplZqUUt8BSooje8UcRnmcAKhPBC6ETLlx21-MiX2yYu9UUnw';
+const vapidPrivateKey = 'sH9tUupLl6h31NYnCeP60s6Acvm_rbIwC2LxxSQpe7k';
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
@@ -46,7 +54,51 @@ exports.savePost = functions.https.onRequest((req, res) => {
 				location,
 			})
 			.then(function () {
+				webPush.setVapidDetails('mailto:ceo@nilanjandeb.me', vapidPublicKey, vapidPrivateKey);
+				return db.ref('subscriptions').once('value');
+			})
+			.then(function (subscriptions) {
+				subscriptions.forEach(function (sub) {
+					const pushConfig = {
+						endpoint: sub.val().endpoint,
+						keys: {
+							auth: sub.val().keys.auth,
+							p256dh: sub.val().keys.p256dh,
+						},
+					};
+					webPush
+						.sendNotification(
+							pushConfig,
+							JSON.stringify({
+								title: 'New Post Added',
+								content: `New Moment ${req.body.title} shared at ${req.body.location}`,
+								postPhoto: req.body.image,
+								openURL: '/help',
+							})
+						)
+						.catch(function (err) {
+							console.log(
+								'Logged Output ~ file: index.js ~ line 70 ~ Push Notification Error',
+								err
+							);
+						});
+				});
 				return res.status(200).json({ message: 'Post Saved!', id: req.body.id });
+			})
+			.catch(function () {
+				return res.status(500).json({ error: 'Something went wrong on the server :/' });
+			});
+	});
+});
+
+exports.addNewSubscription = functions.https.onRequest((req, res) => {
+	cors(req, res, function () {
+		if (!req.body.subscription)
+			return res.status(400).json({ error: 'Subscription request not valid' });
+		db.ref('subscriptions')
+			.push(req.body.subscription)
+			.then(function () {
+				return res.status(200).json({ message: 'Subscribed Successfully!' });
 			})
 			.catch(function () {
 				return res.status(500).json({ error: 'Something went wrong on the server :/' });
